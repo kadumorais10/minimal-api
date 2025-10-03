22f788b534803ace1012cd88128914d7b7e538f7
kadumorais10 <166945868+kadumorais10@users.noreply.github.com>
2025-10-03 10:05:26 -0300

    Delete Api directory

--- Api/Dominio/ModelViews/Home.cs
+++
@@ -1,7 +0,0 @@
-namespace MinimalApi.Dominio.ModelViews;
-
-public struct Home
-{
-    public string Mensagem { get => "bem vindo a api de veiculos - Minimal API"; }
-    public string Doc { get => "/swagger"; }
-}
